208aea2295e89fe31a38920b9ac7ed88322b04ab
Daniel Hartnell <danielhartnell@gmail.com>
2017-03-15 13:24:56 -0700

    copy MediaWiki settings to web root - alongside MediaWiki site content

--- nubis/puppet/copy-site.pp
+++ nubis/puppet/copy-site.pp
@@ -1,17 +1,9 @@
 # Copy index page to Apache web root
 
-file { '/var/www/html/index.htm':
-	ensure => file,
-	owner	 => root,
-	group  => root,
-	mode   => '0755',
-	source => 'puppet:///nubis/files/index.htm',
-}
-
 file { '/var/www/html/LocalSettings.php':
-	ensure => file,
-	owner	 => root,
-	group  => root,
-	mode   => '0744',
-	source => 'puppet:///nubis/files/LocalSettings.php',
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0744',
+  source => 'puppet:///nubis/files/LocalSettings.php',
 }
