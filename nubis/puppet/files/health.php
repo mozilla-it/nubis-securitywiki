d8738f59f1d3a15197aed314b89adbfd4dde19bb
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-03-22 10:23:01 -0400

    php format

--- nubis/puppet/files/health.php
+++ nubis/puppet/files/health.php
@@ -2,30 +2,28 @@
 
 require_once("/etc/nubis-config/securitywiki.php");
 
-$conn = mysql_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD);
+$conn = mysql_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD);
 
 if (!$conn) {
-  http_response_code(500);
-  echo "Can't connect to the database";
-  exit;
+    http_response_code(500);
+    echo "Can't connect to the database";
+    exit;
 }
 
-$DB_NAME="foo";
-
-$db   = mysql_select_db($DB_NAME);
+$db = mysql_select_db($DB_NAME);
 
 if (!$db) {
-  http_response_code(500);
-  echo "Can't find the database $DB_NAME";
-  exit;
+    http_response_code(500);
+    echo "Can't find the database $DB_NAME";
+    exit;
 }
 
 $ping = mysql_ping();
 
 if (!$ping) {
-  http_response_code(500);
-  echo "Database ping failed";
-  exit;
+    http_response_code(500);
+    echo "Database ping failed";
+    exit;
 }
 
 echo "All Checks : OK";
