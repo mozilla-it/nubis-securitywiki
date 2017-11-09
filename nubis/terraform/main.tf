9c20f6b2fd6d821522c8010b16d279ae8f72f388
eziegenhorn <eric@ziegenhorns.com>
2017-11-09 15:12:35 -0600

    use health.php for health checks

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -16,7 +16,7 @@ module "load_balancer" {
   environment         = "${var.environment}"
   account             = "${var.account}"
   service_name        = "${var.service_name}"
-  health_check_target = "HTTP:80/?redirect=0"
+  health_check_target = "HTTP:80/health.php"
   ssl_cert_name_prefix = "securitywiki"
 }
 
