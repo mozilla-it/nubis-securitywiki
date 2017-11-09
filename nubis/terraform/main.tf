67e9c95cba607dc96cac02139b41b2f4dcaff70f
eziegenhorn <eric@ziegenhorns.com>
2017-11-09 12:29:23 -0600

    Reenable normal health checks

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -8,7 +8,6 @@ module "worker" {
   ami               = "${var.ami}"
   elb               = "${module.load_balancer.name}"
   nubis_sudo_groups = "team_webops,nubis_global_admins"
-  health_check_type = "EC2"
 }
 
 module "load_balancer" {
@@ -17,8 +16,7 @@ module "load_balancer" {
   environment         = "${var.environment}"
   account             = "${var.account}"
   service_name        = "${var.service_name}"
-#  health_check_target = "HTTP:80/health.php"
-  health_check_target = "TCP:80"
+  health_check_target = "HTTP:80/health.php"
   ssl_cert_name_prefix = "securitywiki"
 }
 
