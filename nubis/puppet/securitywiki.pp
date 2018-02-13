9d2dafb60f1101e9361818960301944fd0144406
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-13 11:24:48 -0500

    add missed PHP dependencies

--- nubis/puppet/securitywiki.pp
+++ nubis/puppet/securitywiki.pp
@@ -9,8 +9,33 @@ package { 'php-memcache':
   ensure => 'latest'
 }
 
+package { 'php-mbstring':
+  ensure => 'latest'
+}
+
+package { 'php-xml':
+  ensure => 'latest'
+}
+
 package { 'composer':
   ensure => 'latest',
+}
+
+## Install PHP composer extensions
+exec { 'composer':
+    command     => 'composer install --no-dev --verbose',
+    cwd         => "/var/www/${project_name}",
+    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    environment => [
+        'HOME=/tmp',
+    ],
+    require     => [
+      Class['apache::mod::php'],
+      Package['php-memcache'],
+      Package['php-mysql'],
+      Package['php-mbstring'],
+      Package['php-xml'],
+    ],
 }
 
 # Use Nubis's autoconfiguration hooks to trigger out config reloads
