4905be5e68fc76b8d1669d51937de77508d7c245
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-04-11 11:05:25 -0400

    try tip of develop

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -26,7 +26,7 @@ module "load_balancer" {
 }
 
 module "database" {
-  source                 = "github.com/nubisproject/nubis-terraform//database?ref=v2.2.0"
+  source                 = "github.com/nubisproject/nubis-terraform//database?ref=develop"
   region                 = "${var.region}"
   environment            = "${var.environment}"
   account                = "${var.account}"
